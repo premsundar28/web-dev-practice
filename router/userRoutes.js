ca3b3954d2c87b34f677a9555c7b0578aa844a4b
premsundar28 <108226805+premsundar28@users.noreply.github.com>
2025-07-06 13:10:37 +0530

    json token generation completed

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -1,27 +1,38 @@
 const {Router} = require('express');
 const {userModel} = require('../db');
-const jwt = require('jsonwebtoken');
-
+const jwt = require('jsonwebtoken');    
+const bcrypt = require('bcrypt'); 
 const dotenv = require('dotenv');
 
-const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 
 const router = Router(); 
 
 router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
 
-    const {email, password} = req.body;
-
-    await userModel.create({
-        email: email,
-        password : password
-    })  
-   
-    res.json({
-        success: true,
-        message: 'User registered successfully',
+    try {
         
-    })
-})
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        
+        const user = await userModel.create({
+            email,
+            password: hashedPassword
+        });
+
+        
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_USER_PASSWORD,
+            { expiresIn: '1h' } 
+        );
+
+        res.json({ token });
+
+    } catch (error) {
+        console.log("Error in user registration", error);
+        res.status(500).json({ message: error.message });
+    }
+});
 
 module.exports = router;
